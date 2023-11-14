76040a3d980ee371d95fec13b8f2085921e3c31a
Rômulo <romulo.deyvid@hotmail.com>
2023-11-14 19:35:07 -0300

    Atualização no arquivo JS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let numeroSecreto = geraNumeroAleatorio();
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2})
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
 }
 
 
